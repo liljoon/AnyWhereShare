8d1064c6434947e4e4c0830e172b537040dea3b8
milkbottle0305 <bigtog0602@gmail.com>
2023-04-10 12:02:41 +0900

    feat: django rest-auth로 회원가입과 로그인 기능 추가
    rest_framework의 권한을 TokenAuthentication으로 주고, rest-auth에서 superuser로 로그인 시 토큰 발급

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import AccountInfoSerializer, AccountDetailSerializer
+from .models import Account
+
+@api_view(['GET'])
+def list_accounts(request):
+    accounts = Account.objects.all()
+    serializer = AccountInfoSerializer(accounts, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def signup_accounts(request):
+    serializer = AccountDetailSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data)
+
+@api_view(['GET'])
+def detail_accounts(request, account_pk):
+    account = get_object_or_404(Account, pk=account_pk)
+    serializer = AccountDetailSerializer(account)
+    return Response(serializer.data)
