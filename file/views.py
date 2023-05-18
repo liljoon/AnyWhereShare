5a431100a1215858fba849ef66729706807022b2
milkbottle0305 <bigtog0602@gmail.com>
2023-05-18 14:15:19 +0900

    feat: boto3을 통한 s3 코드 구현

--- file/views.py
+++ file/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+import boto3
 
-# Create your views here.
+def s3_connection():
+    try:
+        # s3 클라이언트 생성
+        s3 = boto3.client(
+            service_name="s3",
+            region_name="ap-northeast-2",
+            aws_access_key_id=ACCESS_KEY_ID,
+            aws_secret_access_key=ACCESS_SECRET_KEY,
+        )
+    except Exception as e:
+        print(e)
+    else:
+        print("s3 bucket connected!")
+        return s3
+
+
+def get_list(prefix):
+    response = s3.list_objects(Bucket=BUCKET_NAME, Prefix=prefix)
+    contents_list = response['Contents']
+    file_list = []
+    for content in contents_list:
+        key = content['Key']
+        size = content['Size']
+        file_list.append((key, size))
+    # 파일명 출력
+    for file in file_list:
+        print(file)
+
+
+if __name__ == "__main__":
+    s3 = s3_connection()
+    try:
+        get_list('bigtog/')
+        # s3.download_file(BUCKET_NAME, "test.txt", './help.txt')
+        s3.put_object(Bucket=BUCKET_NAME, Key="bigtog/test")
+    except Exception as e:
+        print(e)
