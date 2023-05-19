1a79861719129ffc03de5c70093bb8addd4f8acc
sunwooiljoon <wnsdl1659@naver.com>
2023-05-19 22:22:45 +0900

    feat : make api for shorten url and find with code

--- share/models.py
+++ share/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class sharing(models.Model):
+	qr_img_b64 = models.TextField()
+	url_origin = models.URLField()
+	url = models.URLField()
+	code = models.CharField(max_length=10)
+	create_date = models.DateTimeField(auto_now_add=True)
--- share/urls.py
+++ share/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+# router = routers.DefaultRouter()
+# router.register('sharing', views.sharingViewSet)
+
 urlpatterns = [
-    path('', views.showShareLink.as_view(), name='showShareLink'),
+	#path('', views.showShareLink.as_view(), name='showShareLink'),
+	#path('', include(router.urls)),
+	path('test/', views.sharingAPIView.as_view())
 ]
