8f6bbaf10743601cebb169f3b81ab3417f6af479
shang001002@gmail.com <evan8816@163.com>
2023-06-01 21:20:53 +0900

    database0601 commit

--- config/settings.py
+++ config/settings.py
@@ -97,8 +97,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'anywhere', #dbname
+        'HOST': 'database-2.clxvqfgb3dhh.us-east-1.rds.amazonaws.com',
+        'PORT': 3306,
+        'USER': 'aw_user', #username
+        'PASSWORD': 'ab12345!',
     }
 }
 
